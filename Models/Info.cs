1f330a3b77718e6e47c5e0a6477f1780da5d34e8
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-05-16 11:53:34 -0300

    homecontroller+

--- Models/Info.cs
+++ Models/Info.cs
@@ -8,11 +8,11 @@ public static class Info{
     
     private static List<Pais> listaPaises {get;set;} = new List<Pais>();
     private static void InicializarLista(){
-        Pais Vanuatu = new Pais("Vanuatu", "", 300000, new DateTime(1930,03,12) , "Esqui artificial");
-        Pais España = new Pais("España" ,"" ,47000000, new DateTime(1808,2,5) , "Playas y Monumentos historicos");
-        Pais Argentina = new Pais("Argentina" ,"" ,46000000, new DateTime(1816,7,9) , "Patagonia"); 
-        Pais Bolivia = new Pais("Bolivia" ,"" ,12000000, new DateTime(1818, 5, 12) , "Salar de Uyuni");
-        Pais Brasil = new Pais("Brasil" ,"" ,215000000, new DateTime(1822,9,7) , "Playas");
+        Pais Vanuatu = new Pais("Vanuatu", "vanuatu.png", 300000, new DateTime(1930,03,12) , "Esqui artificial");
+        Pais España = new Pais("España" ,"españa.png" ,47000000, new DateTime(1808,2,5) , "Playas y Monumentos historicos");
+        Pais Argentina = new Pais("Argentina" ,"argentina.png" ,46000000, new DateTime(1816,7,9) , "Patagonia"); 
+        Pais Bolivia = new Pais("Bolivia" ,"bolivia.png" ,12000000, new DateTime(1818, 5, 12) , "Salar de Uyuni");
+        Pais Brasil = new Pais("Brasil" ,"brasil.png" ,215000000, new DateTime(1822,9,7) , "Playas");
 
         listaPaises.Add(Vanuatu);
         listaPaises.Add(España);
