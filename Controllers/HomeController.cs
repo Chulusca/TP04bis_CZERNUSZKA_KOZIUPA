c3fcdb4477a4ee6b6c1e0a57960b738fe9f3d894
TOMAS CZERNUSZKA SCHIRMER <47569637@ort>
2023-05-16 12:00:59 -0300

    ioajfdi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using TP04bis_CZERNUSZKA_KOZIUPA.Models;
 namespace TP04bis_CZERNUSZKA_KOZIUPA.Controllers;
 
 public class HomeController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        ViewBag.listaPaises = Info.ListarPais();
+        return View("");
+    }
+    public IActionResult DetallePais(string pais){
+        ViewBag.Pais = Info.DetallePais(pais);
+        return View("detallePais.cshtml");
     }
 }
